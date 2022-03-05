997f666bd16531ecee29fd17cc18e0b91d836c4a
johyunjin0905@gmail.com <kkadangj@gmail.com>
2022-03-05 23:32:36 +0900

    Updates

---
+++ component---src-pages-page-2-js-ea4574864bd9ec0c6eeb.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[617],{6034:function(t,e,a){a.r(e);a(7294);var l=a(1597),u=a(5592),n=a(262),o=a(3431);e.default=function(){return(0,o.tZ)(u.Z,null,(0,o.tZ)(n.Z,{title:"Page two"}),(0,o.tZ)("h1",null,"Hi from the second page"),(0,o.tZ)("p",null,"Welcome to page 2"),(0,o.tZ)(l.rU,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-ea4574864bd9ec0c6eeb.js.map
